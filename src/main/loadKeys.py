9f810a22cc5af7369204d515d5094cb6d49a2748
Shakthi Vel <vshakthi@hotmail.com>
2024-12-10 14:26:31 -0800

    Added API fetching

--- src/main/loadKeys.py
+++ src/main/loadKeys.py
@@ -2,5 +2,4 @@ import os
 
 # Access your API keys directly from the environment variables
 VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
-SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
-CVE_API_KEY = os.getenv("CVE_API_KEY")
+ALIENVAULT_API_KEY = os.getenv("ALIENVAULT_API_KEY")
