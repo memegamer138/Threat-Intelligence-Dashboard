d1391491873fbea76efc3b5b53e6051ecbdf6aa7
Shakthi Vel <vshakthi@hotmail.com>
2024-12-10 12:45:34 -0800

    Renmaed for ease of access

--- loadKeys.py
+++ loadKeys.py
@@ -6,6 +6,6 @@ SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
 CVE_API_KEY = os.getenv("CVE_API_KEY")
 
 # Uncomment the following lines to print the API keys. ONLY FOR DEBUGGING PURPOSES
-#print(f"VirusTotal API Key: {VIRUSTOTAL_API_KEY}")
-#print(f"Shodan API Key: {SHODAN_API_KEY}")
-#print(f"CVE API Key: {CVE_API_KEY}")
+print(f"VirusTotal API Key: {VIRUSTOTAL_API_KEY}")
+print(f"Shodan API Key: {SHODAN_API_KEY}")
+print(f"CVE API Key: {CVE_API_KEY}")
